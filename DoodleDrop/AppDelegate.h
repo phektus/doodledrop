74fd67dae58367e798a7a73d59f1293b419e9631
Arbie Samong <phektus@gmail.com>
2012-08-02 19:38:31 +0800

    Initial Commit

---
+++ DoodleDrop/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  DoodleDrop
+//
+//  Created by Arbie Samong on 8/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ DoodleDrop/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  DoodleDrop
+//
+//  Created by Arbie Samong on 8/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
